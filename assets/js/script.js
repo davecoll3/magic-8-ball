5906b42b4d970dada51fc9aaea39dcded36321ee
Daithí <davecoll3@gmail.com>
2022-12-21 14:32:02 +0000

    feat: button click triggered by enter key

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,36 +6,56 @@
 **/
 
 // Selectors for elements on the page to interact with
+let question = document.querySelector('#userQuestion');
 let button = document.querySelector('#shakeButton');
 let answer = document.querySelector('#answer');
+
 let options = [
   'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes definitely.',
+  'You may rely on it.',
   'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Yes',
+  'Signs point to yes.',
+  'Reply hazy, try again.',
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
   'Do not count on it.',
-  'Reply hazy, try again.'
+  'My reply is no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.'
 ]
-
-// Generate a random number
-function generateAnswer() {
-  let index = Math.floor(Math.random() * 4);
-  let message = options[index];
-  answer.textContent = message;
-  answer.style.fontSize = '18px';
-};
 
 //Event listener for button click
 button.addEventListener('click', generateAnswer);
+question.addEventListener("keypress", enterButton);
 
-/* Clicking shake button provides answer
-function submitQuestion() {
-  document.getElementById('answer').innerHTML = 'test'
-}
-*/
+// Generate a random number
+function generateAnswer() {
+  let index = Math.floor(Math.random() * 20);
+  let message = options[index];
+  answer.textContent = message;
+  answer.style.fontSize = '18px';
+  setTimeout(timeOut, 3000);
+};
 
-//Random answers
-// 0 -> I.
-// 1 -> 
-// 2 -> 
-// 3 -> 
+// Timeout function
+function timeOut() {
+  answer.textContent = '8';
+  answer.style.fontSize = '120px';
+};
 
-// Display the answer in place of 8 ball
+// Enter button trigers click event
+function enterButton (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+};
